ec97fa8d22ea2b36336be9f3fce2ae5bc7111aa8
Sneha <snhthakare@gmail.com>
2022-01-28 20:26:37 +0530

    Adds new pages and routes

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 app.use(express.static("public"))
 
+var aboutContent = "This is a daily journal app to track your habits, feelings, changes and thoughts"
+var contactContent = "Email us at abc@xyz.com"
+const homeContent = "Daily"
+
 app.get("/", (req, res) => {
-    res.render("home")
+    res.render("home", { homeStartingContent: homeContent })
+})
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+app.get("/contact", (req, res) => {
+    res.render("contact")
 })
 
 app.listen(3000)
