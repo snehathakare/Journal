95e98da9b2017cf74c1e90e83a531a784703a777
Sneha <snhthakare@gmail.com>
2022-01-29 15:40:26 +0530

    Adds styles to pages

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 app.use(express.static("public"))
 
-var aboutContent = "This is a daily journal app to track your habits, feelings, changes and thoughts"
+var aboutContent = "You can achieve more with less. The Five Minute Journal to bring gratitude into your life. This will help you seamlessly create positive habits without investing years in experimenting and research to achieve the same results."
 var contactContent = "Email us at abc@xyz.com"
-const homeContent = "Daily"
+const homeContent = "'Become everything that one is capable of becoming.' -ABRAHAM MASLOW"
+const composeContent = "A weekly boost of inspiration sent to your inbox. Join 190,000+ people who receive Intelligent Change Weekly–our curated newsletter of personal development tips on happiness, productivity, relationships, and more."
+
 
 app.get("/", (req, res) => {
-    res.render("home", { homeStartingContent: homeContent })
+    res.render("home", { startingContent: homeContent })
 })
 app.get("/about", (req, res) => {
-    res.render("about")
+    res.render("about", { startingContent: aboutContent })
 })
 app.get("/contact", (req, res) => {
-    res.render("contact")
+    res.render("contact", { startingContent: contactContent })
+})
+app.get("/new", (req, res) => {
+    res.render("compose", { startingContent: composeContent })
 })
 
 app.listen(3000)
